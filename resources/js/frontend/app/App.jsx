a61dce0fb01e745958f1a938b3a3466ddc9b54a5
rockmaks_00 <rockmaks_00@mail.ru>
2024-03-06 20:26:59 +0500

    рефакторинг архитектуры

--- resources/js/frontend/app/App.jsx
+++ resources/js/frontend/app/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './router.jsx'
-import { ContextProvider } from './contexts/ContextProvider.jsx'
+import router from './Router.jsx'
+import { ContextProvider } from '../src/contexts/ContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- resources/js/frontend/src/contexts/ContextProvider.jsx
+++ resources/js/frontend/src/contexts/ContextProvider.jsx
@@ -1,35 +1,41 @@
+import propTypes from "prop-types";
 import { createContext, useContext, useState } from "react";
 
 const StateContext = createContext({
-    user: null,
-    token: null,
-    setUser: () => { },
-    setToken: () => { },
+  user: null,
+  token: null,
+  setUser: () => { },
+  setToken: () => { },
 });
 
 export const ContextProvider = ({ children }) => {
-    const [user, setUser] = useState({});
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+  const [user, setUser] = useState({});
+  const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
 
-    const setToken = (token) => {
-        _setToken(token)
-        if (token) {
-            localStorage.setItem('ACCESS_TOKEN', token)
-        } else {
-            localStorage.removeItem('ACCESS_TOKEN')
-        }
+  const setToken = (token) => {
+    _setToken(token);
+    if (token) {
+      localStorage.setItem("ACCESS_TOKEN", token);
+    } else {
+      localStorage.removeItem("ACCESS_TOKEN");
     }
+  };
 
-    return (
-        <StateContext.Provider value={{
-            user,
-            token,
-            setUser,
-            setToken
-        }}>
-            {children}
-        </StateContext.Provider>
-    )
+  return (
+    <StateContext.Provider
+      value={{
+        user,
+        token,
+        setUser,
+        setToken,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
 };
 
-export const useStateContext = () => useContext(StateContext)
+ContextProvider.propTypes = {
+  children: propTypes.node,
+};
+export const useStateContext = () => useContext(StateContext);
